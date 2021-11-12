dc91188f5da9ac5abe74486dfdeb03aac5f5ffa5
Figo <wf851128@gmail.com>
2021-11-12 17:09:18 +0800

    配置信息读取通过 struct 来获取，增加配置变更监控，并设置回调

--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"go_template/logger"
+	"go_template/settings"
 	"net/http"
 )
 
@@ -11,7 +12,7 @@ func Setup() (g *gin.Engine) {
 	r.Use(logger.GinLogger(), logger.GinRecovery(true))
 
 	r.GET("/", func(context *gin.Context) {
-		context.String(http.StatusOK, "ok")
+		context.String(http.StatusOK, settings.Conf.Version)
 	})
 	return r
 }
--- settings/settings.go
+++ settings/settings.go
@@ -6,6 +6,47 @@ import (
 	"log"
 )
 
+var Conf = new(Config)
+
+type Config struct {
+	*AppConfig   `mapstructure:"app"`
+	*LogConfig   `mapstructure:"log"`
+	*MysqlConfig `mapstructure:"mysql"`
+	*RedisConfig `mapstructure:"redis"`
+}
+type AppConfig struct {
+	Name    string `mapstructure:"name"`
+	Mode    string `mapstructure:"mode"`
+	Port    int    `mapstructure:"port"`
+	Version string `mapstructure:"version"`
+}
+
+type LogConfig struct {
+	Level     string `mapstructure:"level"`
+	FileName  string `mapstructure:"file_name"`
+	MaxSize   int    `mapstructure:"max_size"`
+	MaxAge    int    `mapstructure:"max_age"`
+	MaxBackup int    `mapstructure:"max_backup"`
+}
+
+type MysqlConfig struct {
+	Host        string `mapstructure:"host"`
+	Port        int    `mapstructure:"port"`
+	User        string `mapstructure:"user"`
+	Password    string `mapstructure:"password"`
+	DBName      string `mapstructure:"db_name"`
+	MaxOpenConn int    `mapstructure:"max_open_conn"`
+	MaxIdleConn int    `mapstructure:"max_id_conn"`
+}
+
+type RedisConfig struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	DB       int    `mapstructure:"db"`
+	Password string `mapstructure:"password"`
+	PoolSize int    `mapstructure:"pool_size"`
+}
+
 func Init() (err error) {
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
@@ -14,9 +55,16 @@ func Init() (err error) {
 		log.Printf("viper.ReadInConfig failed, err :%s", err)
 		return err
 	}
+	if err = viper.Unmarshal(Conf); err != nil {
+		log.Printf("viper.Unmarshal failed, err :%s", err)
+		return err
+	}
 	viper.WatchConfig()
 	viper.OnConfigChange(func(in fsnotify.Event) {
 		log.Println("配置文件修改了!")
+		if err = viper.Unmarshal(Conf); err != nil {
+			log.Printf("viper.Unmarshal failed, err :%s", err)
+		}
 	})
 	return nil
 }
